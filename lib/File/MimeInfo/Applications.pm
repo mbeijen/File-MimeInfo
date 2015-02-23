80e503d32a807ee385523c32035dfe484d6b83eb
Michiel Beijen <michiel.beijen@gmail.com>
2015-02-23 22:19:46 +0100

    Bump version

--- lib/File/MimeInfo/Applications.pm
+++ lib/File/MimeInfo/Applications.pm
@@ -8,7 +8,7 @@ use File::MimeInfo qw/mimetype_canon mimetype_isa/;
 use File::DesktopEntry;
 require Exporter;
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(
