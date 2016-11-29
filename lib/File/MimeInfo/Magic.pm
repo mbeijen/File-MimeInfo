3d762e20946c923973106464b742dee1a4a31dd8
Michiel Beijen <michiel.beijen@gmail.com>
2016-11-29 08:35:48 +0100

    Add version report test

--- lib/File/MimeInfo/Magic.pm
+++ lib/File/MimeInfo/Magic.pm
@@ -17,7 +17,7 @@ BEGIN {
 our @ISA = qw(Exporter File::MimeInfo);
 our @EXPORT = qw(mimetype);
 our @EXPORT_OK = qw(extensions describe globs inodetype magic);
-our $VERSION = '0.28';
+our $VERSION = '0.28_01';
 our $DEBUG;
 
 our $_hashed = 0;
