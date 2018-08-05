f506640efab2ce086c797c23c44dedb1489efe08
Michiel Beijen <michiel.beijen@gmail.com>
2018-08-05 11:55:57 +0200

    Update for version 0.29

--- lib/File/MimeInfo/Applications.pm
+++ lib/File/MimeInfo/Applications.pm
@@ -8,7 +8,7 @@ use File::MimeInfo qw/mimetype_canon mimetype_isa/;
 use File::DesktopEntry;
 require Exporter;
 
-our $VERSION = '0.28_03';
+our $VERSION = '0.29';
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(
