7607d98ec519c834926afce7a95c192ab88fec78
Michiel Beijen <michiel.beijen@gmail.com>
2020-09-27 16:13:45 +0200

    Update version to 0.29_01 and update Changes file

--- lib/File/MimeInfo/Magic.pm
+++ lib/File/MimeInfo/Magic.pm
@@ -17,7 +17,7 @@ BEGIN {
 our @ISA = qw(Exporter File::MimeInfo);
 our @EXPORT = qw(mimetype);
 our @EXPORT_OK = qw(extensions describe globs inodetype magic);
-our $VERSION = '0.29';
+our $VERSION = '0.29_01';
 our $DEBUG;
 
 our $_hashed = 0;
