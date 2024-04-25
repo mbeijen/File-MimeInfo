f39bfafa0987bb0023cb5495f788617e3ec9f6e9
Michiel W. Beijen <mb@x14.nl>
2024-04-25 20:19:36 +0200

    Release engineering for 0.35

--- lib/File/MimeInfo/Applications.pm
+++ lib/File/MimeInfo/Applications.pm
@@ -9,7 +9,7 @@ use File::MimeInfo qw/mimetype_canon mimetype_isa/;
 use File::DesktopEntry;
 require Exporter;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(
--- lib/File/MimeInfo/Rox.pm
+++ lib/File/MimeInfo/Rox.pm
@@ -11,7 +11,7 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw(mime_exec mime_system);
 our @EXPORT_OK = qw(suggest_script_name);
 our %EXPORT_TAGS = (magic => \@EXPORT);
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 our @choicespath = (
     config_home('rox.sourceforge.net'),
     File::Spec->catdir($ENV{HOME}, 'Choices'),
